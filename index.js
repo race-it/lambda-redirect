38e65da974033d79c32735d3d0ba9884087f9d42
John Jones <john.jones@williamhill.com>
2018-04-19 13:09:09 +0200

    Adding initial files.

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+exports.handler = (event, context, callback) => {
+  /*
+   * Generate HTTP redirect response with 301 status code and Location header.
+   */
+   
+   const request = event.Records[0].cf.request;
+   
+   // get the original URL path
+   const path = request.uri
+   const baseURI = "http://www.baseurl.com"
+
+   const response = {
+      status: '301',
+      statusDescription: 'Found',
+      headers: {
+          location: [{
+              key: 'Location',
+              value: baseURI,
+          }],
+      },
+  };
+  
+  const data = {
+    "mappings" : [
+      {
+        "old" : '/path/to/old/resource',
+        "new" : '/path/to/new/resource'
+      },
+      {
+        "old" : '/path/to/other/old/resource',
+        "new" : '/path/to/other/new/place'
+      }
+    ]
+  };
+
+  var objFound = data.mappings.find(obj => obj.old === path);
+  response.headers.location[0].value = baseURI + objFound.new;
+  
+  callback(null, response);
+};
+
